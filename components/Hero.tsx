b07a58d6dfe9abbfa71685463148e3b010a721e1
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-04 16:22:02 -0700

    Update Hero text color to match yellow gradient styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,8 @@ export default function Hero() {
   return (
     <div className="text-center sm:mt-7">
       <h1 className="text-4xl xs:text-5xl sm:text-5xl md:text-7xl font-medium capitalize tracking-tight text-gray-700 md:mb-8">
-        Turn your <span className="text-blue-500 ">fridge</span> into a{" "}
-        <span className="text-blue-500 ">meal</span>
+        Turn your <span className="text-yellow-500">fridge</span> into a{" "}
+        <span className="text-yellow-500">meal</span>
       </h1>
       <p className="text-gray-600 text-xs md:text-lg max-w-2xl mx-auto">
         Upload a photo of your ingredients and let AI suggest personalized
