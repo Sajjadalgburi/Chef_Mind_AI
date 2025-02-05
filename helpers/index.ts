6224c7bdc32cd76e633f326d05b48916dd851301
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-05 15:31:22 -0700

    Add helper function for retrieving embedding metadata from Pinecone

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,3 +1,4 @@
+// Generate an embedding for a list of ingredient names
 export const generateIngredientEmbedding = async (
   ingredientNames: string[]
 ) => {
@@ -23,6 +24,29 @@ export const generateIngredientEmbedding = async (
     }
 
     return { embedding };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Send the query vector to Pinecone and get the metadata clossest to the query vector
+export const getEmbeddingMetaData = async (queryVector: Array<number>) => {
+  try {
+    const res = await fetch("/api/pinecone-embeddings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ queryVector }),
+    });
+
+    const { metadata } = await res.json();
+
+    if (!metadata) {
+      return { error: "No metadata found" };
+    }
+
+    return metadata;
   } catch (error) {
     console.error(error);
   }
