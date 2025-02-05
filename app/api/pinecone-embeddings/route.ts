bce1085270d1c5a1f47c589fe59276492378e6d4
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-05 15:31:36 -0700

    Create Pinecone embeddings API route for querying food dataset

---
+++ app/api/pinecone-embeddings/route.ts
@@ -0,0 +1,35 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { NextRequest, NextResponse } from "next/server";
+
+const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!,
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const { queryVector } = await req.json();
+    if (!queryVector) {
+      return NextResponse.json(
+        { error: "No query vector provided" },
+        { status: 400 }
+      );
+    }
+    console.log("---- Pinecone Embeddings Endpoint Hit ----");
+
+    const results = await pinecone.index("food-dataset").query({
+      vector: queryVector,
+      topK: 10, // return top 10 results
+      includeMetadata: true,
+    });
+
+    console.log("---- results ----", results);
+
+    const metadata = results.matches.map((match) => match.metadata);
+
+    console.log("---- metadata ----", metadata);
+
+    return NextResponse.json({ metadata }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+  }
+}
