e8b3720bc05a1a58795b366bd51bc8d12cf060e3
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-06 09:53:16 -0700

    Add generateMealPlan helper function for AI-powered meal planning

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,4 +1,10 @@
-// Generate an embedding for a list of ingredient names
+import { GenerateMealPlanProps } from "@/types";
+
+/**
+ * Generate an embedding for a list of ingredient names
+ * @param ingredientNames
+ * @returns
+ */
 export const generateIngredientEmbedding = async (
   ingredientNames: Array<{ category: string; name: string }>
 ) => {
@@ -23,7 +29,11 @@ export const generateIngredientEmbedding = async (
   }
 };
 
-// Send the query vector to Pinecone and get the metadata clossest to the query vector
+/**
+ * Send the query vector to Pinecone and get the metadata clossest to the query vector
+ * @param queryVector
+ * @returns
+ */
 export const getEmbeddingMetaData = async (queryVector: Array<number>) => {
   try {
     const res = await fetch("/api/pinecone-embeddings", {
@@ -41,6 +51,41 @@ export const getEmbeddingMetaData = async (queryVector: Array<number>) => {
     }
 
     return metadata;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Generate a meal plan based on the ingredients
+ * @param param0
+ * @returns
+ */
+export const generateMealPlan = async ({
+  setMealPlanLoading,
+  setRecipes,
+  ingredients,
+  metadata,
+}: GenerateMealPlanProps) => {
+  try {
+    setMealPlanLoading(true);
+
+    const res = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ingredients }),
+    });
+
+    const { AiMealPlan } = await res.json();
+
+    if (!AiMealPlan) {
+      return { error: "No meal plan found" };
+    }
+
+    setRecipes(AiMealPlan);
+    setMealPlanLoading(false);
   } catch (error) {
     console.error(error);
   }
