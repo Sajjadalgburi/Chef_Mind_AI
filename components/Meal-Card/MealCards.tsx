0ae30783c104ec04974b080812adc4d88b7fa63d
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-06 12:39:11 -0700

    Refactor MealCards component rendering logic and loading state

--- components/Meal-Card/MealCards.tsx
+++ components/Meal-Card/MealCards.tsx
@@ -7,7 +7,7 @@ type Props = {
   recipes: MealPlanResponse["recipes"];
 };
 
-const MealCards: React.FC<Props> = ({ isMealPlanLoading = true, recipes }) => {
+const MealCards: React.FC<Props> = ({ isMealPlanLoading, recipes }) => {
   const [visibleSkeletons, setVisibleSkeletons] = useState(1);
 
   useEffect(() => {
@@ -22,25 +22,26 @@ const MealCards: React.FC<Props> = ({ isMealPlanLoading = true, recipes }) => {
     }
   }, [isMealPlanLoading]);
 
-  return (
+  return !isMealPlanLoading ? (
     <div className="gap-8 mx-auto px-4 w-full mb-[5rem] sm:mb-0 py-6 sm:py-[5rem] flex flex-col items-center">
       <div className="w-full max-w-7xl">
         <h1 className="md:text-6xl text-left mb-8 font-bold text-gray-800">
-          {!isMealPlanLoading
-            ? "Your Personalized Meal Plan"
-            : "Crafting your meal plan..."}
+          Your Personalized Meal Plan
         </h1>
 
-        {!isMealPlanLoading ? (
-          <MealCardBody recipes={recipes} />
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-            {Array.from({ length: visibleSkeletons }).map((_, index) => (
-              <LoadingCard key={index} />
-            ))}
-          </div>
-        )}
+        <MealCardBody recipes={recipes} />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+          {Array.from({ length: visibleSkeletons }).map((_, index) => (
+            <LoadingCard key={index} />
+          ))}
+        </div>
       </div>
+    </div>
+  ) : (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+      {Array.from({ length: visibleSkeletons }).map((_, index) => (
+        <LoadingCard key={index} />
+      ))}
     </div>
   );
 };
