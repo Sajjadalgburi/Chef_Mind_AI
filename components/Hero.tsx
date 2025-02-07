4d9caa06e356d17adeda2c601a37fdef61a530f7
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-07 08:52:27 -0700

    Add image prop to Hero component and conditionally hide description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,15 @@
-export default function Hero() {
+export default function Hero({ image }: { image: string }) {
   return (
     <div className="text-center sm:mt-7">
       <h1 className="text-4xl xs:text-5xl sm:text-5xl md:text-7xl font-medium capitalize tracking-tight text-gray-700 md:mb-8">
         Turn your <span className="text-yellow-500">fridge</span> into a{" "}
         <span className="text-yellow-500">meal</span>
       </h1>
-      <p className="text-gray-600 text-xs md:text-lg max-w-2xl mx-auto">
+      <p
+        className={`text-gray-600 text-xs md:text-lg max-w-2xl mx-auto ${
+          image ? "hidden" : ""
+        }`}
+      >
         Upload a photo of your ingredients and let AI suggest personalized
         recipes. No more wasted food, just delicious meals.
       </p>
