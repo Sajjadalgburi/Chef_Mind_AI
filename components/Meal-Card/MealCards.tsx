1f4fbb1b0262f4389250f8e41fd1aa701523ba85
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-07 09:30:55 -0700

    Refactor MealCards component to improve loading state handling and enhance layout structure

--- components/Meal-Card/MealCards.tsx
+++ components/Meal-Card/MealCards.tsx
@@ -8,40 +8,46 @@ import PlaceholderCardBody from "./PlaceholderCardBody";
 const testing = false;
 
 const MealCards: React.FC<MealCardsProps> = ({
-  isMealPlanLoading,
+  isMealPlanLoading = true,
   recipes,
 }) => {
   const [visibleSkeletons, setVisibleSkeletons] = useState(1);
 
   useEffect(() => {
+    if (!recipes && !isMealPlanLoading) return;
+
     if (isMealPlanLoading) {
       const interval = setInterval(() => {
         setVisibleSkeletons((prev) => (prev < 8 ? prev + 1 : prev));
-      }, 2000); // Add new skeleton every 2 seconds
+      }, 5500);
 
       return () => clearInterval(interval);
     } else {
       setVisibleSkeletons(1);
     }
-  }, [isMealPlanLoading]);
+  }, [isMealPlanLoading, recipes]);
 
-  return !isMealPlanLoading ? (
-    <div className="gap-8 mx-auto px-4 w-full mb-[5rem] sm:mb-0 py-6 sm:py-[5rem] flex flex-col items-center">
-      <div className="w-full max-w-7xl">
-        <h1 className="md:text-6xl text-left mb-8 font-bold text-gray-800">
-          Your Personalized Meal Plan
-        </h1>
+  return (
+    <div className="gap-8 mx-auto px-4 mb-[5rem] sm:mb-0 py-6 sm:py-[5rem] flex flex-col items-center">
+      {!isMealPlanLoading ? (
+        <div className="w-full max-w-[80vw]">
+          <h1 className="md:text-6xl text-left mb-8 font-bold text-gray-800">
+            Your Personalized Meal Plan
+          </h1>
 
-        {testing ? <PlaceholderCardBody /> : <MealCardBody recipes={recipes} />}
-      </div>
-    </div>
-  ) : (
-    <div className="gap-8 mx-auto px-4 w-full mb-[5rem] sm:mb-0 py-6 sm:py-[5rem] flex flex-col items-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-        {Array.from({ length: visibleSkeletons }).map((_, index) => (
-          <LoadingCard key={index} />
-        ))}
-      </div>
+          {testing ? (
+            <PlaceholderCardBody />
+          ) : (
+            <MealCardBody recipes={recipes} />
+          )}
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+          {Array.from({ length: visibleSkeletons }).map((_, index) => (
+            <LoadingCard key={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
