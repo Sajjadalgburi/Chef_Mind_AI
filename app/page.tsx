d5d090587dff9f1da03745c17f7583ede7ed43cb
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-08 13:32:20 -0700

    Refactor page layout and remove Footer component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Hero from "../components/Hero";
 import ImageUploadSection from "../components/ImageUploadSection";
-import Footer from "../components/Footer";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { handleGenerate } from "@/helpers";
@@ -52,30 +51,28 @@ export default function Home() {
   };
 
   return (
-    <main className="min-h-screen flex flex-col justify-between items-center mx-auto bg-gradient-to-b">
-      <div className="w-full max-w-4xl">
+    <main className="min-h-screen flex flex-col items-center w-full bg-gradient-to-b">
+      <section className="w-full max-w-7xl px-4 flex flex-col items-center">
         <Toaster position="top-center" reverseOrder={false} />
-      </div>
 
-      {showResults ? (
-        <MealCards
-          isMealPlanLoading={isMealPlanLoading}
-          recipes={recipes as MealPlanResponse["recipes"]}
-        />
-      ) : (
-        <div className="flex flex-col items-center justify-between gap-4 px-4 w-full text-center mb-[5rem] sm:mb-0">
-          <Hero image={image as string} />
-
-          <ImageUploadSection
-            setImage={setImage}
-            image={image}
-            loading={loading}
-            handleImageUpload={handleImageUpload}
-            handleGenerate={handleGenerateClick}
+        {showResults ? (
+          <MealCards
+            isMealPlanLoading={isMealPlanLoading}
+            recipes={recipes as MealPlanResponse["recipes"]}
           />
-        </div>
-      )}
-      <Footer />
+        ) : (
+          <div className="w-full flex flex-col items-center gap-8 py-8">
+            <Hero image={image as string} />
+            <ImageUploadSection
+              setImage={setImage}
+              image={image}
+              loading={loading}
+              handleImageUpload={handleImageUpload}
+              handleGenerate={handleGenerateClick}
+            />
+          </div>
+        )}
+      </section>
     </main>
   );
 }
