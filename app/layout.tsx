ebbec9a11f077eb65a14b165c922a53f512afa52
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-08 13:32:13 -0700

    Add Footer component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function RootLayout({
           <>
             <Header />
             {children}
+            <Footer />
           </>
         </body>
       </html>
