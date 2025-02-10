681abdb75cbc398e64741f07cc72cfa6414c36dc
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-10 16:57:39 -0700

    Add AuthForm component for login and registration UI

---
+++ components/AuthForm.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Label } from "./ui/label";
+import Link from "next/link";
+import { Input } from "./ui/input";
+import { SubmitButton } from "./submit-button";
+import { FormMessage, Message } from "./form-message";
+import { signInAction, signUpAction } from "@/actions/actions";
+
+type AuthFormProps = {
+  searchParams: Message;
+  type: "login" | "register";
+};
+
+const AuthForm = async (props: AuthFormProps) => {
+  const { searchParams } = props;
+
+  return (
+    <>
+      <h1 className="sm:text-4xl font-2xl font-bold">
+        {props.type === "login" ? "Login" : "Register"}
+      </h1>
+
+      <p className="text-sm text-foreground">
+        {props.type === "login"
+          ? "Don&apos;t have an account? "
+          : "Already have an account? "}
+        <Link
+          className="text-primary font-medium underline"
+          href={props.type === "login" ? "/sign-up" : "/sign-in"}
+        >
+          {props.type === "login" ? "Register" : "Login"}
+        </Link>
+      </p>
+      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+        {/* Only shows on register */}
+        {props.type === "register" && (
+          <>
+            <Label htmlFor="username">Username</Label>
+            <Input name="username" placeholder="you@example.com" required />
+          </>
+        )}
+
+        <Label htmlFor="email">Email</Label>
+        <Input name="email" placeholder="you@example.com" required />
+
+        <div className="flex justify-between items-center">
+          <Label htmlFor="password">Password</Label>
+          <Link
+            className="text-xs text-foreground underline"
+            href="/forgot-password"
+          >
+            Forgot Password?
+          </Link>
+        </div>
+
+        <Input
+          type="password"
+          name="password"
+          placeholder="Your password"
+          required
+        />
+
+        {props.type === "login" ? (
+          <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+            Login
+          </SubmitButton>
+        ) : (
+          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
+            Register
+          </SubmitButton>
+        )}
+
+        <FormMessage message={searchParams} />
+      </div>
+    </>
+  );
+};
+
+export default AuthForm;
