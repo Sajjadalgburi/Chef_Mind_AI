037a93942485295f322f7f755805fbf6b8ef8815
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-10 19:38:05 -0700

    Update FormMessage component styling and text alignment

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -5,19 +5,20 @@ export type Message =
 
 export function FormMessage({ message }: { message: Message }) {
   return (
-    <div className="flex flex-col gap-2 w-full max-w-md text-sm">
+    <div className="flex flex-col gap-2 text-center w-full max-w-md text-sm">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
+        <div className="border-l-2 border-foreground px-4">
           {message.success}
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="border-l-2 border-destructive-foreground px-4">
           {message.error}
         </div>
       )}
+
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className="border-l-2 px-4">{message.message}</div>
       )}
     </div>
   );
