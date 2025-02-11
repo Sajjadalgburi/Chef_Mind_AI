5b88a5a42b38c28d046452ed87bc952750d3bf74
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-10 19:38:19 -0700

    Wrap root layout with AuthContextProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/navbar/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { AuthContextProvider } from "@/context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,11 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <>
+        <AuthContextProvider>
           <Header />
           {children}
           <Footer />
-        </>
+        </AuthContextProvider>
         <Analytics />
       </body>
     </html>
