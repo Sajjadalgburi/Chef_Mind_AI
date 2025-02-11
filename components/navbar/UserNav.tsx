e5cc06678b1a393ae1a4f39dd808854c4bf95257
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-10 19:12:49 -0700

    Refactor UserNav component with loading state and conditional rendering

--- components/navbar/UserNav.tsx
+++ components/navbar/UserNav.tsx
@@ -5,57 +5,67 @@ import Link from "next/link";
 import { useUser } from "@/hooks";
 import { Loader2 } from "lucide-react";
 import { signOutAction } from "@/actions/actions";
+import { useEffect, useState } from "react";
 
 const UserNav = () => {
   const { user, loading } = useUser();
+  const [isPageLoading, setIsPageLoading] = useState(true);
 
-  if (!user) {
-    return (
-      <div className="flex items-center gap-2">
-        <Button
-          asChild
-          className="text-olive rounded-sm bg-olive/10 hover:bg-olive/20 text-sm md:text-xl"
-        >
-          <Link href="/sign-in">Sign In</Link>
-        </Button>
+  useEffect(() => {
+    setIsPageLoading(false);
+  }, []);
 
-        <Button
-          asChild
-          className="bg-terracotta text-white rounded-sm hover:bg-terracotta/90 text-sm md:text-xl"
-        >
-          <Link href="/sign-up">Get Started</Link>
-        </Button>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex items-center gap-2">
-      {loading ? (
-        <div>
-          <Loader2 className="w-4 h-4 animate-spin" />
-        </div>
-      ) : (
+  return isPageLoading ? (
+    <div className="flex items-center justify-center h-full gap-2">
+      <Loader2 className="md:w-12 md:h-12 w-8 h-8 text-olive animate-spin" />
+    </div>
+  ) : (
+    <>
+      {user !== null ? (
         <div className="flex items-center gap-2">
-          {/* <Image
+          {loading ? (
+            <div>
+              <Loader2 className="w-4 h-4 animate-spin" />
+            </div>
+          ) : (
+            <div className="flex items-center gap-2">
+              {/* <Image
             src={user.imageUrl}
             alt={user.username}
             width={32}
             height={32}
           /> */}
+              <Button
+                asChild
+                className="text-olive bg-olive/10 text-sm md:text-xl rounded-sm hover:bg-olive/20 transition-colors duration-200"
+              >
+                <Link href="/profile">Profile</Link>
+              </Button>
+
+              <Button className="text-sm md:text-xl" onClick={signOutAction}>
+                Logout
+              </Button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex items-center gap-2">
           <Button
             asChild
-            className="text-olive bg-olive/10 text-sm md:text-xl rounded-sm hover:bg-olive/20 transition-colors duration-200"
+            className="text-olive rounded-sm bg-olive/10 hover:bg-olive/20 text-sm md:text-xl"
           >
-            <Link href="/profile">Profile</Link>
+            <Link href="/sign-in">Sign In</Link>
           </Button>
 
-          <Button className="text-sm md:text-xl" onClick={signOutAction}>
-            Logout
+          <Button
+            asChild
+            className="bg-terracotta text-white rounded-sm hover:bg-terracotta/90 text-sm md:text-xl"
+          >
+            <Link href="/sign-up">Get Started</Link>
           </Button>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
