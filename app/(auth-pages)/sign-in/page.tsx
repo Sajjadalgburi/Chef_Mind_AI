fa68fb2382f1dc59fecfddeb7ddb67f418b06d78
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-10 17:34:19 -0700

    Update Login page to handle async searchParams

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,8 +1,8 @@
 import { Message } from "@/components/form-message";
 import AuthForm from "@/components/AuthForm";
 
-export default function Login(props: { searchParams: Message }) {
-  const { searchParams } = props;
+export default async function Login(props: { searchParams: Promise<Message> }) {
+  const searchParams = await props.searchParams;
 
   return <AuthForm searchParams={searchParams} type="login" />;
 }
