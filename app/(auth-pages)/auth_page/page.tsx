496b7f6fcc75fb0213b1384a6c131935caf688c6
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-11 17:39:48 -0700

    Consolidate auth pages into a single dynamic auth form page

---
+++ app/(auth-pages)/auth_page/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "@/components/AuthForm";
+
+export default async function Signup() {
+  return <AuthForm />;
+}
--- app/(auth-pages)/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Message } from "@/components/form-message";
-import AuthForm from "@/components/AuthForm";
-
-export default async function Login(props: { searchParams: Promise<Message> }) {
-  const searchParams = await props.searchParams;
-
-  return <AuthForm searchParams={searchParams} type="login" />;
-}
