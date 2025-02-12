7545fa1b4d933ea0e0bff1c715da388c96b1039b
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-11 18:43:06 -0700

    Add useAuth hook for accessing AuthContext

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,11 @@
-"use client";
-
 import { AuthContext } from "@/context/AuthContext";
 import { useContext } from "react";
 
-export const useUser = () => {
+export default function useAuth() {
   const context = useContext(AuthContext);
-
   if (context === undefined) {
-    throw new Error("useUser must be used within a AuthContextProvider.");
+    throw new Error("useAuth must be used within an AuthProvider");
   }
 
   return context;
-};
+}
