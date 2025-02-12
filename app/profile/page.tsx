d570a2f334ef9fd5ca7753cb890911f2923713c9
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-12 11:37:52 -0700

    Add loading skeleton and improve user recipes display on Profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,36 +5,31 @@ import React, { useState, useEffect } from "react";
 import { getUserRecipes } from "@/actions";
 import useAuth from "@/hooks/useAuth";
 import { getSupabaseClient } from "@/config/supbaseClient";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Profile = () => {
-  // const [dislikedRecipes, setDislikedRecipes] = useState<string[]>([]);
   const [userRecipes, setUserRecipes] = useState<MealPlanResponse["recipes"]>(
     []
   );
-
-  // const handleSave = (recipe: MealPlanResponse["recipes"][0]) => {};
-
-  // const handleDislike = (recipeTitle: string) => {};
+  const [loading, setLoading] = useState(true);
 
   const { user, session } = useAuth();
   const userId = user?.id as string;
-  console.log(userId);
 
   useEffect(() => {
     if (!userId || !session) return;
 
     const { supabaseAccessToken } = session;
 
-    if (!supabaseAccessToken || getSupabaseClient(supabaseAccessToken)) return;
+    const supabaseClient = getSupabaseClient(supabaseAccessToken as string);
+
+    if (!supabaseAccessToken || supabaseClient) return;
 
     const fetchUserRecipes = async () => {
-      const recipes = await getUserRecipes(
-        userId,
-        getSupabaseClient(supabaseAccessToken)
-      );
+      const recipes = await getUserRecipes(userId, supabaseClient);
 
       if (!recipes) return;
-
+      setLoading(false);
       setUserRecipes(recipes as unknown as MealPlanResponse["recipes"]);
     };
 
@@ -51,28 +46,21 @@ const Profile = () => {
           Easily access your favorite meal plans anytime.
         </p>
 
-        {!userRecipes.length ? (
+        {loading ? (
           <div className="mt-10 text-center">
-            <h2 className="text-2xl font-semibold text-gray-700">
-              No saved meals yet!
-            </h2>
-            <p className="text-gray-500">
-              Start exploring and save meals for quick access.
+            <Skeleton className="h-96 w-full" />
+          </div>
+        ) : userRecipes.length > 0 ? (
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 w-full">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+              expedita cumque facere corporis, quos voluptas consectetur est
+              soluta iste perferendis incidunt error autem illo ab nostrum
+              laudantium minus culpa fuga.
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 w-full">
-            {/* {userRecipes.map((recipe, index) => (
-              <RecipeCard
-                key={index}
-                recipe={recipe}
-                isSaved={userRecipes.some((r) => r.title === recipe.title)}
-                isDisliked={dislikedRecipes.includes(recipe.title)}
-                onSave={handleSave}
-                onDislike={handleDislike}
-              />
-            ))} */}
-          </div>
+          <div>No saved meals yet!</div>
         )}
       </div>
     </section>
