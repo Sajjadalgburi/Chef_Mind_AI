92c64bcff0c51cbb4cbbcb59f06f8dcae316b56b
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-12 09:20:21 -0700

    Implement API rate limiting middleware using Upstash Redis

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
 import { auth } from "@/app/auth";
 import { NextRequest, NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
 
-export { auth } from "@/app/auth";
+/**
+ * Checks if the path is an API route
+ * @param path - The path to check
+ * @returns True if the path is an API route, false otherwise
+ */
+const isAPI = (path: string) => {
+  return path.startsWith("/api/") || path.startsWith("/app/api/");
+};
 
 export const protectedRoutes = [
   "/api/generate-meal-plan",
@@ -12,10 +21,30 @@ export const protectedRoutes = [
   "/profile",
 ];
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL ?? "",
+  token: process.env.UPSTASH_REDIS_REST_TOKEN ?? "",
+});
+
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(10, "86400 s"), // 10 requests per day
+  ephemeralCache: new Map(),
+  analytics: true,
+});
+
+if (
+  !process.env.UPSTASH_REDIS_REST_URL ||
+  !process.env.UPSTASH_REDIS_REST_TOKEN
+) {
+  throw new Error(
+    "UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN must be set in the environment variables"
+  );
+}
+
 export default async function middleware(req: NextRequest) {
   const { nextUrl } = req;
   const session = await auth();
-
   const pathname = nextUrl.pathname;
 
   const isProtected = protectedRoutes.some((route) => {
@@ -26,5 +55,31 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/auth_page", nextUrl));
   }
 
+  //Rate limit APIs
+  if (isAPI(pathname)) {
+    const userId = session?.user?.id as string;
+    const { success, limit, reset, remaining } = await ratelimit.limit(
+      `${userId}`
+    );
+
+    const res = success
+      ? NextResponse.next()
+      : NextResponse.json(
+          { errorMessage: "Rate limit exceeded. Please try again later." },
+          { status: 429 }
+        );
+
+    res.headers.set("X-RateLimit-Limit", limit.toString());
+    res.headers.set("X-RateLimit-Remaining", remaining.toString());
+    res.headers.set("X-RateLimit-Reset", reset.toString());
+
+    if (!success) return res;
+    return res;
+  }
+
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
