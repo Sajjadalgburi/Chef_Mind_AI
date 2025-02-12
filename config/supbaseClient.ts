754608c4f98a33b57e9cad7b1aaaeb2ed2fbb638
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-12 11:29:27 -0700

    Refactor Supabase client to support dynamic token injection

--- config/supbaseClient.ts
+++ config/supbaseClient.ts
@@ -1,27 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/chef_mind_types";
-import { auth } from "@/app/auth";
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-  throw new Error("SUPABASE_URL and SUPABASE_ANON_KEY must be set");
-}
-
-const session = await auth();
-
-if (!session) {
-  throw new Error("Session not found");
-}
-
-const { supabaseAccessToken } = session;
-
-export const supabaseClient = createClient<Database>(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!,
-  {
-    global: {
-      headers: {
-        Authorization: `Bearer ${supabaseAccessToken}`,
+export const getSupabaseClient = (supabaseAccessToken: string) => {
+  return createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
       },
-    },
-  }
-);
+    }
+  );
+};
