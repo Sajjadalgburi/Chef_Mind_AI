3167371dd23d4a6f0dacb6c0cf01eaf5730f420e
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-11 18:43:48 -0700

    Update signInWithProvider to use callbackUrl instead of redirectTo

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,10 +2,8 @@
 
 import { signIn, signOut } from "@/app/auth";
 
-export const signInWithProvider = async (
-  provider: "github" | "google" = "google"
-) => {
-  await signIn(provider, { redirectTo: "/" });
+export const signInWithProvider = async (provider: "github" | "google") => {
+  await signIn(provider, { callbackUrl: "/" });
 };
 
 export const logout = async () => {
