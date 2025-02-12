1661733e9b33c5f8267f60fa1204b970c4f1bdef
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-12 09:39:57 -0700

    Add user details to AuthContext for enhanced session management

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -3,8 +3,16 @@
 import { createContext, ReactNode } from "react";
 import { Session } from "next-auth";
 
+export type UserProps = {
+  id: string | null | undefined;
+  email: string | null | undefined;
+  name: string | null | undefined;
+  image: string | null | undefined;
+};
+
 type AuthContextType = {
   session: Session | null;
+  user: UserProps | null | undefined;
 };
 
 export const AuthContext = createContext<AuthContextType | undefined>(
@@ -14,11 +22,15 @@ export const AuthContext = createContext<AuthContextType | undefined>(
 export function AuthProvider({
   children,
   session,
+  user,
 }: {
   children: ReactNode;
   session: Session | null;
+  user: UserProps | null;
 }) {
   return (
-    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
