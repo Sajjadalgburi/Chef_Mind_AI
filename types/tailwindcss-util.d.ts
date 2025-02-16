ab66c1f3f768be2cfda6f4665793f6861724ad45
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-02-16 15:18:38 -0700

    Add TypeScript type definition for Tailwind's flattenColorPalette utility

---
+++ types/tailwindcss-util.d.ts
@@ -0,0 +1,5 @@
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const flattenColorPalette: (colors: any) => any;
+  export default flattenColorPalette;
+}
